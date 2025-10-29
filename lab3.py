1a44c6174cf59320b84f2b7f9f557ebc865d6df8
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2025-10-29 21:08:09 +0100

    Create lab3.py

---
+++ lab3.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+import control
+
+def kalmanMatrix(A,B):
+    n = A.shape[0]
+    kalman = B
+    for i in range(1,n):
+        kalman = np.hstack((kalman, np.linalg.matrix_power(A,i)@B))
+    print(kalman)
+    print(np.linalg.matrix_rank(kalman))
+
+def simulation(A,B,C, controlls, titles, labels):
+    D = np.array([[0]])
+    sys = signal.StateSpace(A,B,C,D)
+    t = np.linspace(0,10,500)
+    for i in range(len(controlls)):
+        plt.figure()
+        plt.title(titles[i])
+        u = np.array([controlls[i](j) for j in t])
+        tout, yout, xout = signal.lsim(sys, U=u, T=t)
+        plt.plot(tout, xout, label=labels)
+        plt.legend()
+    plt.show()
+
+def posacSterowalna(A,B,C):
+    D = np.array([[0]])
+    sys = control.StateSpace(A,B,C,D)
+    sys2,T = control.canonical_form(sys, form='reachable')
+    return sys2
+
+
+
+def ones(t):
+    return 1
+
+def doubleOnes(t):
+    return 2
+
+def sinsub05(t):
+    return np.sin(t)-0.5
+
+#Ukl 1
+A = np.array([[-0.5, 0],[0, -0.5]])
+B = np.array([[0.5],[0.5]])
+C = np.array([[1,0]])
+#kalmanMatrix(A,B)
+#simulation(A,B,C,[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2"])
+
+#Ukl 2
+A = np.array([[-1,0,0],[0,-0.5,0],[0,0,-1.0/3]])
+B = np.array([[1],[0.5],[1.0/3]])
+C = np.array([[1,0,0]])
+#kalmanMatrix(A,B)
+#simulation(A,B,C,[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
+sys = posacSterowalna(A,B,C)
+print(sys.A)
+print(sys.B)
+print(sys.C)
+simulation(sys.A, sys.B, sys.C,[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
+
+#Ukl 3
+A = np.array([[0,10.0,0,0],[-10,-10,0,-10],[0,0,0,10],[0,-10,-10,-10]])
+B = np.array([[0],[10.0],[0],[10]])
+C = np.array([[1,0,0,0]])
+#kalmanMatrix(A,B)
+#simulation(A,B,C,[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3","x4"])
+
+#Ukl 4
+A = np.array([[-4,0,-2],[0,0,1],[0.5,-0.5,-0.5]])
+B = np.array([[2.0],[0],[0]])
+C = np.array([[1.0,0,0]])
+#kalmanMatrix(A,B)
+#simulation(A,B,C,[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
