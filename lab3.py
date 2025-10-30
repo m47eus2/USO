a384ebdc68e9b10dd4da7ec91491ab0118ac7e4e
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2025-10-30 15:10:56 +0100

    Update lab3.py -> warunki poczatkowe

--- lab3.py
+++ lab3.py
@@ -11,7 +11,7 @@ def kalmanMatrix(A,B):
     print(kalman)
     print(np.linalg.matrix_rank(kalman))
 
-def simulation(A,B,C, controlls, titles, labels):
+def simulation(A,B,C, x0, controlls, titles, labels):
     D = np.array([[0]])
     sys = signal.StateSpace(A,B,C,D)
     t = np.linspace(0,10,500)
@@ -20,7 +20,7 @@ def simulation(A,B,C, controlls, titles, labels):
         plt.grid()
         plt.title(titles[i])
         u = np.array([controlls[i](j) for j in t])
-        tout, yout, xout = signal.lsim(sys, U=u, T=t)
+        tout, yout, xout = signal.lsim(sys, U=u, T=t, X0=x0)
         plt.plot(tout, xout, label=labels)
         plt.plot(tout, yout, label="y")
         plt.legend()
@@ -57,7 +57,7 @@ B = np.array([[1],[0.5],[1.0/3]])
 noweA = A-(B @ K)
 noweB = np.array([[0],[0],[0]])
 C = np.array([[1,0,0]])
-simulation(noweA, noweB, C, [ones], ["Nowy układ"], ["x1","x2","x3"])
+simulation(noweA, noweB, C, [10,20,5], [ones], ["Nowy układ"], ["x1","x2","x3"])
 
 #kalmanMatrix(A,B)
 #simulation(A,B,C,[doubleOnes],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
