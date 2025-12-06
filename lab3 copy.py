e26f6b41a63dbdd61500f6e63fb662b70a9c170d
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2025-12-06 14:20:21 +0100

    lab3 copy update

--- lab3 copy.py
+++ lab3 copy.py
@@ -3,14 +3,6 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import control
 
-def kalmanMatrix(A,B):
-    n = A.shape[0]
-    kalman = B
-    for i in range(1,n):
-        kalman = np.hstack((kalman, np.linalg.matrix_power(A,i)@B))
-    print(kalman)
-    print(np.linalg.matrix_rank(kalman))
-
 def simulation(A,B,C, x0, controlls, title, labels):
     D = np.array([[0]])
     sys = signal.StateSpace(A,B,C,D)
@@ -35,8 +27,6 @@ def posacSterowalna(A,B,C):
     sys2,T = control.canonical_form(sys, form='reachable')
     return sys2
 
-
-
 def ones(t):
     return 1
 
@@ -50,39 +40,28 @@ def sinsub05(t):
 A = np.array([[-0.5, 0],[0, -0.5]])
 B = np.array([[0.5],[0.5]])
 C = np.array([[1,0]])
-#kalmanMatrix(A,B)
-#simulation(A,B,C, [0,0],[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2"])
+simulation(A,B,C, [0,0],[ones,doubleOnes,sinsub05],"Przebiegi zmiennych stanu układu z Rys.1 dla różnych wymuszeń", ["x1","x2"])
 
 #Ukl 2
 A = np.array([[-1,0,0],[0,-0.5,0],[0,0,-1.0/3]])
-K = np.array([[9.83,16,6.17]])
 B = np.array([[1],[0.5],[1.0/3]])
-noweA = A-(B @ K)
-noweB = np.array([[0],[0],[0]])
 C = np.array([[1,0,0]])
+#simulation(A,B,C,[0,0,0],[ones, doubleOnes, sinsub05],"Przebiegi zmiennych stanu układu z Rys.2 dla różnych wymuszeń", ["x1","x2","x3"])
 
-#simulation(noweA, noweB, C, [10,20,5], [ones], ["Nowy układ"], ["x1","x2","x3"])
-
-#kalmanMatrix(A,B)
-simulation(A,B,C,[0,0,0],[ones, doubleOnes, sinsub05],"Przebiegi zmiennych stanu układu 2 dla różnych wymuszeń", ["x1","x2","x3"])
 sys = posacSterowalna(A,B,C)
-#print(sys.A)
-#print(sys.B)
-#print(sys.C)
 #simulation(sys.A, sys.B, sys.C,[0,0,0],[doubleOnes],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
 
 #Ukl 3
 A = np.array([[0,10.0,0,0],[-10,-10,0,-10],[0,0,0,10],[0,-10,-10,-10]])
 B = np.array([[0],[10.0],[0],[10]])
 C = np.array([[1,0,0,0]])
-#kalmanMatrix(A,B)
-#simulation(A,B,C,[0,0,0,0],[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3","x4"])
+#simulation(A,B,C,[0,0,0,0],[ones,doubleOnes,sinsub05],"Przebiegi zmiennych stanu układu z Rys.3 dla różnych wymuszeń", ["x1","x2","x3","x4"])
 
 #Ukl 4
 A = np.array([[-4,0,-2],[0,0,1],[0.5,-0.5,-0.5]])
 B = np.array([[2.0],[0],[0]])
 C = np.array([[1.0,0,0]])
-#kalmanMatrix(A,B)
 #simulation(A,B,C,[0,0,0],[ones,doubleOnes,sinsub05],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
 
+plt.savefig("lab3_1_3_1.pdf", format = 'pdf')
 plt.show()
