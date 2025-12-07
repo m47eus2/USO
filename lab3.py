2e8b90f7e3d3c9723d756f50eef53e864103fc1e
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2025-12-07 16:34:09 +0100

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -11,19 +11,22 @@ def kalmanMatrix(A,B):
     print(kalman)
     print(np.linalg.matrix_rank(kalman))
 
-def simulation(A,B,C, x0, controlls, titles, labels):
+def simulation(A,B,C, x0, controlls, titles, labels, showY):
     D = np.array([[0]])
     sys = signal.StateSpace(A,B,C,D)
-    t = np.linspace(0,10,500)
+    t = np.linspace(0,12,500)
     for i in range(len(controlls)):
         plt.figure()
         plt.grid()
+        plt.xlabel("t [s]")
+        plt.ylabel("h(t) []")
         plt.title(titles[i])
         u = np.array([controlls[i](j) for j in t])
         tout, yout, xout = signal.lsim(sys, U=u, T=t, X0=x0)
         plt.plot(tout, xout, label=labels)
-        plt.plot(tout, yout, label="y")
+        if showY: plt.plot(tout, yout, label="y")
         plt.legend()
+        #plt.savefig("lab3_3_3.pdf", format = 'pdf')
     
 
 def posacSterowalna(A,B,C):
@@ -52,21 +55,35 @@ C = np.array([[1,0]])
 
 #Ukl 2
 A = np.array([[-1,0,0],[0,-0.5,0],[0,0,-1.0/3]])
-K = np.array([[9.83,16,6.17]])
 B = np.array([[1],[0.5],[1.0/3]])
-noweA = A-(B @ K)
-noweB = np.array([[0],[0],[0]])
 C = np.array([[1,0,0]])
 
-simulation(noweA, noweB, C, [10,20,5], [ones], ["Nowy układ"], ["x1","x2","x3"])
-
 #kalmanMatrix(A,B)
-#simulation(A,B,C,[0,0,0],[doubleOnes],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
+#simulation(A,B,C,[0,0,0],[doubleOnes],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])\
+#simulation(A,B,C,[0,0,0],[ones],["Odpowiedź na skok jednostkowy dla pierwotnej postaci równań stanu"], ["x1","x2","x3"], 0)
+
+# Postać sterowalna
 sys = posacSterowalna(A,B,C)
-#print(sys.A)
-#print(sys.B)
-#print(sys.C)
-#simulation(sys.A, sys.B, sys.C,[0,0,0],[doubleOnes],["Przebieg stanu dla u=1(t)","Przebieg stanu dla u=2(t)","Przebieg stanu dla u=sin(t)-1/2"], ["x1","x2","x3"])
+print(sys.A)
+print(sys.B)
+print(sys.C)
+Ar = sys.A
+Br = sys.B
+Cr = sys.C
+#simulation(Ar, Br, Cr,[0,0,0],[ones],["Odpowiedź na skok jednostkowy dla równań stanu w postaci sterowalnej"], ["x1","x2","x3"], 1)
+
+# Postać sterowalna w innej formie
+Arr = np.array([[0,1,0],[0,0,1],[-0.17,-1,-1.83]])
+Brr = np.array([[0],[0],[1]])
+Crr = np.array([[0.166, 0.833, 1]])
+#simulation(Arr, Brr, Crr,[0,0,0],[ones],["Odpowiedź na skok jednostkowy dla równań stanu w drugiej postaci sterowalnej"], ["x1","x2","x3"], 1)
+
+K = np.array([[9.83,16,6.17]])
+Az = Ar-(Br @ K)
+Bz = np.array([[0],[0],[0]])
+Cz = Cr
+
+simulation(Az, Bz, Cr, [5,10,15], [ones], ["Odpowiedź układu zamkniętego"], ["x1","x2","x3"],0)
 
 #Ukl 3
 A = np.array([[0,10.0,0,0],[-10,-10,0,-10],[0,0,0,10],[0,-10,-10,-10]])
