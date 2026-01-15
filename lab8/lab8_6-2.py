b83a58386503935ded8656098136e128853dd9b5
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-15 14:50:20 +0100

    Update lab8

---
+++ lab8/lab8_6-2.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from scipy.signal import StateSpace, lsim
+
+l = 0.65
+m = 0.01187
+km = 0.000116
+r = 27.7
+g = 9.81
+
+#Linearyzacja
+x1 = 0.1
+x3 = 3.168
+A = np.array([[0,1,0],[(2*(km/m)*x3**2) / x1**2,0,(-2*km/m)*(x3/x1**2)],[0,(2*km / l)*x3 / x1**2, -r/l]])
+B = np.array([[0],[0],[1/l]])
+
+# Model neliniowy
+
+def control(t):
+    return 1
+
+def model(t,x):
+    dx1 = x[1]
+    dx2 = g - (km/m)*(x[2]**2/x[0]**2)
+    dx3 = (2*km/l)*(x[1]*x[2]/(x[0]**2)) - (r/l)*x[2] + control(t)/l
+    return [dx1, dx2, dx3]
+
+ret = solve_ivp(model, [0,20], [0.1,0,0], rtol=1e-10, atol=1e-10)
+
+plt.figure()
+plt.title("Odpowiedź modelu")
+plt.grid()
+plt.xlabel("t [s]")
+plt.ylabel("h(t) []")
+plt.plot(ret.t, ret.y[0], label="x1")
+plt.plot(ret.t, ret.y[1], label="x2")
+plt.plot(ret.t, ret.y[2], label="x3")
+plt.legend()
+plt.show()
+
+def control2(t):
+    return np.array([[0]])
+
+def model2(t,x):
+    x = np.array([x]).T
+    dx = A @ x + B @ control2(t)
+    return np.ndarray.tolist(dx.T[0])
+
+res = solve_ivp(model2, [0,5], [0,0,0], rtol=1e-10, atol=1e-10)
+
+plt.figure()
+plt.grid()
+plt.plot(res.t, res.y[0], label="x1")
+plt.plot(res.t, res.y[1], label="x2")
+plt.plot(res.t, res.y[2], label="x3")
+plt.show()
--- lab8/lab8_Linearyzacja2.py
+++ lab8/lab8_Linearyzacja2.py
@@ -10,44 +10,6 @@ g=10
 d=0.5
 
 #
-# Pierwszy punkt pracy (0,0)
-#
-
-# Model neliniowy
-
-def control(t):
-    return 5
-
-def model(t,x):
-    dx1 = x[1]
-    dx2 = (1/J)*control(t) - (d/J)*x[1] - ((m*g*l)/J)*np.sin(x[0])
-    return [dx1, dx2]
-
-ret = solve_ivp(model, [0,20], [0,0], rtol=1e-10, atol=1e-10)
-
-plt.figure()
-plt.title("Odpowiedź modelu")
-plt.grid()
-plt.xlabel("t [s]")
-plt.ylabel("h(t) []")
-plt.plot(ret.t, ret.y[0], label="Obiekt nieliniowy")
-
-# Liniowy 1 - linearyzacja w (0,0)
-
-A = np.array([[0,1],[-(m*g*l)/J, -d/J]])
-B = np.array([[0],[1/J]])
-C = np.array([[1, 0]])
-D = 0
-
-sys = StateSpace(A,B,C,D)
-t = np.linspace(0,20,1000)
-u0 = 5
-u = u0 * np.ones_like(t)
-tout, yout, xout = lsim(sys, u, t, X0=[0,0])
-plt.plot(tout, yout, "--", label="Linearyzacja w x=(0,0)")
-plt.legend()
-
-#
 # Drugi punkt pracy (pi/4, 0)
 #
 
