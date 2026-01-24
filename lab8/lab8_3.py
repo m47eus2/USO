087c3834322841a6b19084f24b864e7cc6f8fed8
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-24 18:11:04 +0100

    Update lab8_3.py

--- lab8/lab8_3.py
+++ lab8/lab8_3.py
@@ -9,8 +9,7 @@ J=1
 g=10
 d=0.5
 
-uz = 70
-#uz = 45*np.sqrt(2)
+uz = 45*np.sqrt(2)+10
 
 #
 # Pierwszy punkt pracy (0,0)
@@ -51,37 +50,38 @@ u = uz * np.ones_like(t)
 tout, yout, xout = lsim(sys, u, t, X0=[0,0])
 plt.plot(tout, yout, "--", label="Linearyzacja w x=(0,0)")
 plt.legend()
-plt.savefig("lab8_5_1_4.pdf", format = 'pdf')
-plt.show()
+#plt.savefig("lab8_5_1_4.pdf", format = 'pdf')
+
 
 #
 # Drugi punkt pracy (pi/4, 0)
 #
 
-# Model nieliniowy
-# ret2 = solve_ivp(model, [0,20], [np.pi/4,0], rtol=1e-10, atol=1e-10)
+#Model nieliniowy
+ret2 = solve_ivp(model, [0,20], [np.pi/4,0], rtol=1e-10, atol=1e-10)
 
-# # Liniowy 2 - linearyzacja w (pi/4, 1)
-# plt.figure()
-# plt.title("Odpowiedź modelu")
-# plt.grid()
-# plt.xlabel("t [s]")
-# plt.ylabel("h(t) []")
-# plt.plot(ret2.t, ret2.y[0], label="Obiekt nieliniowy")
 
-# A = np.array([[0,1],[(-(m*g*l)/J)*(np.sqrt(2)/2), -d/J]])
-# B = np.array([[0],[1/J]])
-# C = np.array([[1, 0]])
-# D = 0
+plt.figure()
+plt.title("Model nieliniowy oraz po linearyzacji, wymuszenie u=$45\sqrt{2}+10$")
+plt.grid()
+plt.xlabel("t [s]")
+plt.ylabel("theta(t) [rad]")
+plt.plot(ret2.t, ret2.y[0], label="Obiekt nieliniowy")
 
-# sys = StateSpace(A,B,C,D)
-# t = np.linspace(0,20,1000)
-# u0 = 50 - 45*np.sqrt(2)
-# u = u0 * np.ones_like(t)
-# tout, yout, xout = lsim(sys, u, t, X0=[0,0])
+# Liniowy 2 - linearyzacja w (pi/4, 1)
 
-# plt.grid()
-# plt.plot(tout, yout+(np.pi/4), "--", label="Linearyzacja w x=(pi/4, 0)")
+A = np.array([[0,1],[(-(m*g*l)/J)*(np.sqrt(2)/2), -d/J]])
+B = np.array([[0],[1/J]])
+C = np.array([[1, 0]])
+D = 0
 
-# plt.legend()
-# #plt.show()
+sys = StateSpace(A,B,C,D)
+t = np.linspace(0,20,1000)
+u = uz-45*np.sqrt(2) * np.ones_like(t)
+tout, yout, xout = lsim(sys, u, t, X0=[0,0])
+
+plt.plot(tout, yout+(np.pi/4), "--", label="Linearyzacja w x=(pi/4, 0)")
+
+plt.legend()
+#plt.savefig("lab8_5_2_2.pdf", format = 'pdf')
+plt.show()
